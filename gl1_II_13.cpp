fbd9cdfe462f4aa012d74e56bd881ed3658cf398
Feedonya <145389323+Feedonya@users.noreply.github.com>
2023-09-18 23:13:05 +0400

    Add files via upload

---
+++ gl1_II_13.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    float l;
+    cout << "Введите l: " << endl;
+    cin >> l;
+    cout << "Радиус круга равен: " << l / (M_PI * 2) << endl;
+    return 0;
+}
