5cb0d16592f7a6c14f6175fb20123083dd1f495c
Feedonya <145389323+Feedonya@users.noreply.github.com>
2023-09-19 13:37:11 +0400

    Add files via upload

---
+++ gl3_II_7.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+     day;
+    setlocale(LC_ALL,"Russian");
+    cout << "¬ведите день недели: " << endl;
+    cin >> day;
+    
+	if 
+    {
+    	
+    }
+	else if 
+	{
+		
+	}
+   
+    return 0;
+}
