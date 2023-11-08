04462f7b0a7e2314df257c1ffcdea6dc93397bb0
feedonya <fedor.musatov@yandex.ru>
2023-11-08 23:17:45 +0400

    modified:   gl5_II_9.cpp

--- gl5_II_9.cpp
+++ gl5_II_9.cpp
@@ -5,9 +5,14 @@ using namespace std;
 int main(){
     setlocale(LC_ALL,"Russian");
 
-    int k, S=0;
+    int k, S=0, x;
     cout << "Введите k: ";
     cin >> k;
     
+    for (int n=1; n<=k; ++n){
+        n = 2*n;
+        S += (-1*pow(x,n)/);
+    }
+
     return 0;
 }
