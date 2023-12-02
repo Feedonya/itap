f4533dfc2c9cea457a8073589d671f3640cfee46
feedonya <fedor.musatov@yandex.ru>
2023-12-02 13:30:12 +0400

    deleted:    gl5_III_17 	modified:   gl6_IIII_3.cpp

--- gl6_IIII_3.cpp
+++ gl6_IIII_3.cpp
@@ -2,37 +2,76 @@
 
 using namespace std;
 
-int* insert__in_array(int *arr, int m, int n){ // вставляем элемент после последнего максимального
-    cin >> arr[n+1]; // вставляем желаемый элемент в массив
+void print_array(int **arr, int lines, int columns);
+int * insert_vector_in_array(int **arr, int lines, int columns);
 
-    for (int i = n+1; i > m; --i){
-        arr[i+1] = arr[i];
-    }
-
-    return arr;
-}
 
 int main(){
     setlocale(LC_ALL,"Russian");
 
-    int n1, m;
+    int n, m; //строки, столбцы
     cout << "Введите количество строк двумерного массива: ";
-    cin >> n1;
+    cin >> n;
     cout << "Введите количество столбцов двумерного массива: ";
     cin >> m;
 
-    int ** b = new int *[n1];//инициализируем двумерный динамический массив
-    for (int i=0;i<n1;++i){
+    int ** b = new int *[n]; //инициализируем двумерный динамический массив
+    for (int i=0;i<n;++i){
         b[i] = new int[m];
     }
 
-    for (int i=0; i<n1; ++i){//вводим элементы массива
+    cout << "Введите элементы двумерного массива: ";
+    for (int i=0; i<n; ++i){ //вводим элементы массива
         for (int j=0; j<m; ++j){
             cin >> b[i][j];
         }
     }
 
-    
+    print_array(b,n,m);
+
+    insert_vector_in_array(b,n,m);
+
+    print_array(b,n,m);
 
     return 0;
 }
+
+int* insert_vector_in_array(int **arr, int lines, int columns){ //вставляем вектор, если столбец(column) четный
+    int len_vec;
+    cout << "Введите длину вектора: ";
+    cin >> len_vec;
+
+    cout << "Введите вектор: ";
+    int *vec = new int[len_vec];
+    for (int i = 0; i < len_vec; ++i){ //инициализируем вектор
+        cin >> vec[i];
+    }
+
+    for (int i = 0; i < columns; ++i){ //заменяем четные столбцы на введеный вектор
+        if (i%2==0){
+            for (int j = 0; j < lines; ++j){
+                arr[i][j] = vec[j]; // segmentation error!!! если длина вектора меньше длины столбца -> удаляем элементы четного столбца + вводим шам вектор и приравниванием длину данного столбца на длину вектора.
+            }
+        }
+    }
+
+    return *arr;
+}
+
+void print_array(int **arr, int lines, int columns){//выводим массив
+    for (int i = 0; lines > i; ++i){
+        for (int j = 0; columns > j; ++j){
+            cout << arr[i][j] << " ";
+        }
+    }
+    cout << endl;
+}
+
+/*
+test_input:
+3
+5
+1 2 3 1 2 3 1 2 3 1 2 3 1 2 3
+1
+1
+*/
