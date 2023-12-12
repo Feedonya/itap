9b8b7c9ce37a7a1b18e782fbe029d70f912d73df
feedonya <fedor.musatov@yandex.ru>
2023-12-12 18:22:40 +0400

    modified:   8_1_1.3.cpp 	renamed:    chat_gpt(6_3_3).cpp -> chat/chat_gpt(6_3_3).cpp 	renamed:    gl7_2_2(chat_gpt).cpp -> chat/gl7_2_2(chat_gpt).cpp

--- 8_1_1.3.cpp
+++ 8_1_1.3.cpp
@@ -1,25 +1,33 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
-bool check(unsigned n){
-    unsigned a,b;
-    cout << "Введите a, b через пробел: ";
-    cin >> a >> b;
-    cout << endl;
-    
-    for (int i = 0; i < b; ++i){
-        
-    }
-
+bool prime(int n){
+	for(long long i=2;i<=sqrt(n);i++)
+		if(n%i==0)
+			return false;
+	return true;
 }
 
 int main(){
     setlocale(LC_ALL,"Russian");
     unsigned n;
-    cout << "Введите N:";
+    cout << "Введите N: ";
     cin >> n;
-    check(n);
+    cout << prime(n) << endl;
+
+    unsigned a, b, ans = 0;
+    cout << "Введите a, b через пробел: ";
+    cin >> a >> b;
+    
+
+    for (int i = a; i < b; ++i){
+        if (prime(i))
+            ans += i;
+    }
+
+    cout << ans;
 
     return 0;
 }
