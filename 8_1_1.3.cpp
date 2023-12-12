6d81f848367b567471e3db07e95b7b52a6a4d8d7
Ivan Musatov <aterl@yandex.ru>
2023-12-12 13:40:20 +0400

    new file:   8_1_1.3.cpp

---
+++ 8_1_1.3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+bool check(unsigned n){
+    unsigned a,b;
+    cout << "Введите a, b через пробел: ";
+    cin >> a >> b;
+    cout << endl;
+    
+    for (int i = 0; i < b; ++i){
+        
+    }
+
+}
+
+int main(){
+    setlocale(LC_ALL,"Russian");
+    unsigned n;
+    cout << "Введите N:";
+    cin >> n;
+    check(n);
+
+    return 0;
+}
