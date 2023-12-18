256e5ef73e1bbdd3bfaf080745e5e8a225f4d5ba
feedonya <fedor.musatov@yandex.ru>
2023-12-18 23:04:39 +0400

    modified:   8_2_19.cpp

--- 8_2_19.cpp
+++ 8_2_19.cpp
@@ -2,17 +2,17 @@
 
 using namespace std;
 
-int findMin(int** arr, int rows, int cols, int currentRow, int currentCol, int minVal) {
-    if (currentRow == rows) {
+int findMin(int** arr, int rows, int cols, int currentRow, int currentCol, int minVal){
+    if (currentRow == rows){//крайний случай
         return minVal;
     }
-    if (currentCol == cols) {
+    if (currentCol == cols){//проходим дальше по строке
         return findMin(arr, rows, cols, currentRow + 1, 0, minVal);
     }
-    if (arr[currentRow][currentCol] < minVal) {
+    if (arr[currentRow][currentCol] < minVal){//проверяем элемент на минимальность
         minVal = arr[currentRow][currentCol];
     }
-    return findMin(arr, rows, cols, currentRow, currentCol + 1, minVal);
+    return findMin(arr, rows, cols, currentRow, currentCol + 1, minVal);//проходим дальше по столбцу
 }
 
 int main() {
@@ -23,9 +23,9 @@ int main() {
     cin >> cols;
 
     int** arr = new int*[rows];
-    for (int i = 0; i < rows; i++) {
+    for (int i = 0; i < rows; i++){
         arr[i] = new int[cols];
-        for (int j = 0; j < cols; j++) {
+        for (int j = 0; j < cols; j++){
             cout << "Введите элемент: [" << i << "][" << j << "]: ";
             cin >> arr[i][j];
         }
@@ -34,10 +34,18 @@ int main() {
     int minVal = findMin(arr, rows, cols, 0, 0, arr[0][0]);
     cout << "Минимальное значение в массиве: " << minVal << endl;
 
-    for (int i = 0; i < rows; i++) {
+    for (int i = 0; i < rows; i++){
         delete[] arr[i];
     }
     delete[] arr;
 
     return 0;
 }
+/*
+input:
+3
+2
+1 2 3 4 5 6
+output:
+
+*/
