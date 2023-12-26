d365984eccb5e8dd985a96264c630c16a822f8b8
feedonya <fedor.musatov@yandex.ru>
2023-12-26 16:01:53 +0400

    modified:   8_4_17.cpp

--- 8_4_17.cpp
+++ 8_4_17.cpp
@@ -2,41 +2,41 @@
 
 using namespace std;
 
-float F(float x){
+float F(float x, float a, float b, float c){
+
     if (x < 93.0){
         return a+b*x;
     }
     else if (x < 121.0){
         return b-a*c;
     }
-    else {
+    else{
         return a*b*x;
     }
 
 }
 
-void F(float x, float &y){
-    
+void F(float x, float &y){//выводим значения
+    cout << "Для x = " << x << " Значение y равно: " << y << endl;
 }
 
 int main(){
     setlocale(LC_ALL,"Russian");
 
-    float A,B,h,ans;
+    float A, B, h, y;
 
-    cout << "Введите A,B,h: ";
+    cout << "Введите A, B, h: ";
     cin >> A >> B >> h;
-    
+
     float a,b,c;
-    cout << "Введите a,b,c: ";
+    cout << "Введите a, b, c: ";
     cin >> a >> b >> c;
 
+    for (float x = A; x <= B; x += h){
 
-    for (float i = A; i <= B; i += h){
-        ans = result_func(i,a,b,c);
-        cout << "Для x = " << i << " Значение y равно: " << ans << endl;
+        y = F(x,a,b,c);
+        F(x,y);
     }
 
     return 0;
 }
-
