18774fa48840e97210688874e95e3e6910a82e8b
Ivan Musatov <aterl@yandex.ru>
2023-12-27 10:51:16 +0400

    modified:   8_4_17.cpp 	modified:   8_5_13.cpp

--- 8_4_17.cpp
+++ 8_4_17.cpp
@@ -2,37 +2,54 @@
 
 using namespace std;
 
-float result_func(float x, float a, float b, float c){
+float F(float x, float a, float b, float c){
     if (x < 93.0){
         return a+b*x;
     }
     else if (x < 121.0){
         return b-a*c;
     }
-    else {
+    else{
         return a*b*x;
     }
 
 }
 
+void F(float x, float &y, float a, float b, float c){//выводим значения
+    if (x < 93.0){
+        y = a+b*x;
+    }
+    else if (x < 121.0){
+        y = b-a*c;
+    }
+    else{
+        y = a*b*x;
+    }
+}
+
 int main(){
     setlocale(LC_ALL,"Russian");
 
-    float A,B,h,ans;
+    float A, B, h, y;
 
-    cout << "Введите A,B,h: ";
+    cout << "Введите A, B, h: ";
     cin >> A >> B >> h;
-    
+
     float a,b,c;
-    cout << "Введите a,b,c: ";
+    cout << "Введите a, b, c: ";
     cin >> a >> b >> c;
 
+    for (float x = A; x <= B; x += h){
+        y = F(x,a,b,c);
+        cout <<"Для x = " << x << " значение равно: " << y << endl;
+    }
 
-    for (float i = A; i <= B; i += h){
-        ans = result_func(i,a,b,c);
-        cout << "Для x = " << i << " Значение y равно: " << ans << endl;
+    cout << "\n";
+
+    for (float x = A; x <= B; x += h){
+        F(x,y,a,b,c);
+        cout <<"Для x = " << x << " значение равно: " << y << endl;
     }
 
     return 0;
 }
-
