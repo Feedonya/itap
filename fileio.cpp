c83b4975a6b498f8d9be431bba04fce9d29c9657
Ivan Musatov <aterl@yandex.ru>
2024-02-13 10:53:49 +0400

    modified:   fileio.cpp 	new file:   fileio.exe

--- fileio.cpp
+++ fileio.cpp
@@ -1,8 +1,23 @@
-#include <qstream>
+#include <fstream>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main(){
+    setlocale(LC_ALL,"Russian");
+    ifstream in("input.txt");
+    ofstream out("output.txt");
+    string s;
+    while (in.peek() != EOF){//!in.eof()){
+        getline(in,s);
+        cout << s;
+        out << s << endl;
+    }
     
+    
+
+    in.close();
+    out.close();
+    return 0;
 }
