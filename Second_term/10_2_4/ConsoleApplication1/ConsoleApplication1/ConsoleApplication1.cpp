55ec38d3aa02cae9b298cc842b383f2e79992eed
Fedor <fedor.musatov@yandex.ru>
2024-02-27 11:32:52 +0400

    modified:   Second_term/10_2_4/ConsoleApplication1/.vs/ConsoleApplication1/v16/.suo 	modified:   Second_term/10_2_4/ConsoleApplication1/.vs/ConsoleApplication1/v16/Browse.VC.db 	modified:   Second_term/10_2_4/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp

--- Second_term/10_2_4/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ Second_term/10_2_4/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -4,31 +4,43 @@
 
 using namespace std;
 
-struct point {
-    int x, y, z;
+struct student {
+    string name;
+    int id_group, mark_of_first_exam, mark_of_second_exam, mark_of_third_exam;
+    void delete_student();
 };
 
-
-double point::distance() {
-    return (sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2);
+void student::delete_student() {
+    if (mark_of_first_exam < 3 or mark_of_second_exam < 3 or mark_of_third_exam < 3) {
+        //удалить строчку со студентом
+    }
 }
+
 
 int main() {
     setlocale(LC_ALL, "Russian");
 
     ifstream in("input.txt");
-
-    point coord;
+    ofstream out("output.txt");
+    
+    student name;
+    student mark;
 
     while (in.peek() != EOF) {
-        in >> coord.x;
-        in >> coord.y;
-        in >> coord.z;
-        coord.point();
+
+        in >> 
+
+        in >> mark.mark_of_first_exam;
+        in >> mark.mark_of_second_exam;
+        in >> mark.mark_of_third_exam;
+
+        mark.delete_student();
+
     }
 
 
     in.close();
+    out.close();
 
     return 0;
 }
