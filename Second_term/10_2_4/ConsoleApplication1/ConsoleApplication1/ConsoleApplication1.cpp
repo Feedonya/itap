cd584ef5c8be54a8e4f83a01424bc83f2961bc6a
Fedor <fedor.musatov@yandex.ru>
2024-02-27 11:18:22 +0400

    Added class work from VS

---
+++ Second_term/10_2_4/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,34 @@
+﻿#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct point {
+    int x, y, z;
+};
+
+
+double point::distance() {
+    return (sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2);
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    ifstream in("input.txt");
+
+    point coord;
+
+    while (in.peek() != EOF) {
+        in >> coord.x;
+        in >> coord.y;
+        in >> coord.z;
+        coord.point();
+    }
+
+
+    in.close();
+
+    return 0;
+}
