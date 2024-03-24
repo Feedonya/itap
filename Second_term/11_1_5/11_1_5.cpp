eec8fe6fc91a57ab6b900ec3c1c99f3654709da7
feedonya <fedor.musatov@yandex.ru>
2024-03-24 15:38:48 +0400

    completed 11_1_5

--- Second_term/11_1_5/11_1_5.cpp
+++ Second_term/11_1_5/11_1_5.cpp
@@ -4,33 +4,35 @@
 
 using namespace std;
 
-
-ifstream in("input.txt");
-ofstream out("output.txt");
+//ifstream in("input.txt");
+//ofstream out("output.txt");
+ifstream in("C:\\Users\\Fedor\\Desktop\\itap\\Second_term\\11_1_5\\input.txt");
+ofstream out("C:\\Users\\Fedor\\Desktop\\itap\\Second_term\\11_1_5\\output.txt");
 
 struct student {
     string name_of_student, surname, patronymic;
-    int id_group, mark_of_first_exam, mark_of_second_exam, mark_of_third_exam, mark_of_fourth_exam, mark_of_fifth_exam, date_of_birth;
-    int sum[4];
+    int mark_of_first_exam, mark_of_second_exam, mark_of_third_exam, mark_of_fourth_exam, mark_of_fifth_exam, date_of_birth;
     int sum_of_students();
     void input();
-    void output();
-    void insertionSort(int *, int);
+    void output(int[], int);
+    void insertionSort(int* , int);
 };
 
 void student::input() {
-	in >> id_group >> surname >> name_of_student >> patronymic >> date_of_birth >> mark_of_first_exam >> mark_of_second_exam >> mark_of_third_exam >> mark_of_fourth_exam >> mark_of_fifth_exam;
+	in >> surname >> name_of_student >> patronymic >> date_of_birth >> mark_of_first_exam >> mark_of_second_exam >> mark_of_third_exam >> mark_of_fourth_exam >> mark_of_fifth_exam;
 }
 
-void student::output(){
-	out << sum << '\n';
+void student::output(int arr[], int n){
+    for (int i = 0; i < n; i++)
+	    out << arr[i] << '\n';
 }
 
 int student::sum_of_students() {
+    cout << mark_of_first_exam + mark_of_second_exam + mark_of_third_exam + mark_of_fourth_exam + mark_of_fifth_exam << "\n";
     return (mark_of_first_exam + mark_of_second_exam + mark_of_third_exam + mark_of_fourth_exam + mark_of_fifth_exam);
 }
 
-void student::insertionSort(int arr[], int n) {
+void student::insertionSort(int* arr, int n) {
     for (int i = 1; i < n; ++i) {
         int key = arr[i];
         int j = i - 1;
@@ -46,18 +48,21 @@ void student::insertionSort(int arr[], int n) {
 int main() {
     setlocale(LC_ALL, "Russian");
     student st1;
-    int i = 0;
+
+    int i = 0, group;
+    in >> group;
 
 	int sum[4];
 
-    while (i <= 4) { //? while (in.peek() != EOF) {
+    //for (int j = 0, number_of_students ){}
+    while (i < 4) { //? while (in.peek() != EOF) {
     	st1.input();
     	sum[i] = st1.sum_of_students();
     	i++;
 	}
 	
 	st1.insertionSort(sum, 4);
-	st1.output();
+	st1.output(sum, 4);
 
     in.close();
     out.close();
