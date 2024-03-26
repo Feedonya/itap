c3b2ae1d457c39e7e10cfac18b06b819e19bb4d8
Fedor <fedor.musatov@Yandex.ru>
2024-03-26 09:42:31 +0300

    hotfix 11_1_5

--- Second_term/11_1_5/11_1_5.cpp
+++ Second_term/11_1_5/11_1_5.cpp
@@ -4,10 +4,10 @@
 
 using namespace std;
 
-//ifstream in("input.txt");
-//ofstream out("output.txt");
-ifstream in("C:\\Users\\Fedor\\Desktop\\itap\\Second_term\\11_1_5\\input.txt");
-ofstream out("C:\\Users\\Fedor\\Desktop\\itap\\Second_term\\11_1_5\\output.txt");
+ifstream in("input.txt");
+ofstream out("output.txt");
+//ifstream in("C:\\Users\\Fedor\\Desktop\\itap\\Second_term\\11_1_5\\input.txt");
+//ofstream out("C:\\Users\\Fedor\\Desktop\\itap\\Second_term\\11_1_5\\output.txt");
 
 struct student{
     string name_of_student, surname, patronymic;
@@ -23,8 +23,9 @@ void student::input(){
 }
 
 void student::output(int arr[], int n){
+    //out << surname << name_of_student << patronymic << date_of_birth << mark_of_first_exam << mark_of_second_exam << mark_of_third_exam << mark_of_fourth_exam << mark_of_fifth_exam;
     for (int i = 0; i < n; i++)
-	    out << arr[i] << '\n';
+	out << arr[i] << '\n';
 }
 
 int student::sum_of_students(){
@@ -48,19 +49,18 @@ int main(){
     setlocale(LC_ALL, "Russian");
     student st1;
 
-    int i = 0, group, n;
+    int i = 0, group, n = 20;
     in >> group;
-    cout << "Введите количество студентов группы: "; // 4
-    cin >> n;
 
 	int sum[n];
 
-
-    while (i < n){ 
+    n = 0;
+    while (in.peek() != EOF){
     	st1.input();
     	sum[i] = st1.sum_of_students();
     	i++;
 	}
+    n = i;
 	
 	st1.insertionSort(sum, n);
 	st1.output(sum, n);
