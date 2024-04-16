d8f9e8998d22f3c1a3493276a5bb35042f81e3c5
feedonya <fedor.musatov@yandex.ru>
2024-04-17 01:27:59 +0400

    added 9 class

--- Second_term/class_9/9.cpp
+++ Second_term/class_9/9.cpp
@@ -3,88 +3,102 @@
 
 using namespace std;
 
-class Person {
+class Person{
 private:
-  string name;
-  string second_name;
+    string name;
+    string second_name;
 
 public:
-  Person() {
-    name = "No";
-    second_name = "Name";
-  }
+    Person(){
+		name = "No"; 
+		second_name = "Name";
+    }
 
-  Person(string n, string s) {
-    name = n;
-    second_name = s;
-  }
+    Person(string n, string s){
+		name = n;
+		second_name = s;
+    }
 
-  string GetName() {
-    return name + " " + second_name;
-  }
-  
-	bool operator > (Person s1) {
+    string GetName(){
+		return name + " " + second_name;
+	}
+    
+    bool operator > (Person s1){
 		return name > s1.name;
+	}
+	bool operator < (Person s1){
+		return name < s1.name;
+	}
+	bool operator == (Person s1){
+		return name == s1.name;
+	}
+};
+
+class Song{
+    private:
+		Person performer;
+		Person song_author;
+		Person lyrics_author;
+		string lyrics;
+		static int count;	
+    public:
+    Song(Person p, Person s, Person l, string lyr){
+		performer = p;
+		song_author = s;
+		lyrics_author = l;
+		lyrics = lyr;
+		count++;
+    }
+
+    void ChangeLyrics(string nLyrics){
+    	lyrics = nLyrics;
+    }
+
+    string GetPerformer(){
+    	return performer.GetName();
+    }
+
+    string GetSongAuthor(){
+    	return song_author.GetName();
+    }
+
+    string GetLyricsAuthor(){
+    	return lyrics_author.GetName();
+    }
+
+    string GetLyrics(){
+    	return lyrics;
+    }
+
+	static int GetCount(){
+    	return count;
+    }
+    
+    string GetSongInfo(){
+		string temp;
+		temp.append("Исполнитель: ").append(GetPerformer()).append(" Автор музыки: ").append(GetSongAuthor()).append(" Автор слов: ").append(GetLyricsAuthor()).append(" Текст песни: ").append(GetLyrics());
+		return temp;
   	}
 };
 
-class Song {
-private:
-  Person performer;
-  Person song_author;
-  Person lyrics_author;
-  string lyrics;
-public:
-  Song(Person p, Person s, Person l, string lyr) {
-    performer = p;
-    song_author = s;
-    lyrics_author = l;
-    lyrics = lyr;
-  }
-  void ChangeLyrics(string nLyrics) {
-    lyrics = nLyrics;
-  }
 
-  string GetPerformer() {
-    return performer.GetName();
-  }
+int Song::count = 0;
 
-  string GetSongAuthor() {
-    return song_author.GetName();
-  }
+int main(){
 
-  string GetLyricsAuthor() {
-    return lyrics_author.GetName();
-  }
+	Person p1("Juju", "Baba");
+	Person p2("Sergey", "Lazarev");
 
-  string GetLyrics() {
-    return lyrics;
-  }
-  
-  string GetSongInfo() {
-    return << "Song by " << performer << ". Music author is " << song_author() << ". Lyrics author is " << lyrics_author() << "Lyrics:" << endl << lyrics;;
-}
-};
+	Song s1(p1, p1, p1, "bbebebebebebebebebebeebebebeb");
+	Song s2(p1, p1, p1, "aaaaaaaaa alalalalalal");
+	Song s3(p2, p2, p2, "eifioqfoqhfjqbkjfbjkqbfjkqbfbqfkbj");
 
+	cout << (p1 > p2);
+	cout << '\n';
 
+	cout << s3.GetCount() << '\n';
+	
+	cout << s1.GetSongInfo();
 
-
-int main() {
-
-  Person p1("Juju", "Baba");
-  Person p2("Sergey", "Lazarev");
-
-  Song s1(p1, p1, p1, "bbebebebebebebebebebeebebebeb");
-  Song s2(p1, p1, p1, "aaaaaaaaa alalalalalal");
-  Song s3(p2, p2, p2, "eifioqfoqhfjqbkjfbjkqbfjkqbfbqfkbj");
-
-  cout << (s1 > s2).GetPerformer();
-  cout << '\n';
-  cout << (s1 > s3).GetPerformer();
-
-  cout << endl << endl;
-
-  GetSongInfo(s1);
-
-  return 0;
+	return 0;
 }
