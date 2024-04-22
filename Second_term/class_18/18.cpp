d44c2ac4b7b4c6fd9f6456d5ceb488f286293913
feedonya <fedor.musatov@yandex.ru>
2024-04-22 22:48:40 +0400

    added 18

--- Second_term/class_18/18.cpp
+++ Second_term/class_18/18.cpp
@@ -1,13 +1,61 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-class name(){
-    
+class Enemy {
+protected:
+    string name;
+    int attackPower;
+
+public:
+    // Конструктор
+    Enemy(const string& _name, int _attackPower) : name(_name), attackPower(_attackPower) {}
+
+    virtual void attack() const = 0;
+
+    // Виртуальный деструктор
+    virtual ~Enemy() {}
 };
 
-int main(){
-    setlocale(LC_ALL,"Russian");
+class Lancer : public Enemy {
+public:
+    // Конструктор
+    Lancer(const string& _name, int _attackPower) : Enemy(_name, _attackPower) {}
+
+    // Переопределение функции attack
+    void attack() const override {
+        cout << "Рыцарь " << name << " атакует" << endl;
+    }
+};
+
+// Класс Dragon, наследующийся от Enemy
+class Dragon : public Enemy {
+public:
+    // Конструктор
+    Dragon(const string& _name, int _attackPower) : Enemy(_name, _attackPower) {}
+
+    // Переопределение функции attack
+    void attack() const override {
+        cout << "Дракон " << name << " атакует" << endl;
+    }
+};
+
+int main() {
+    // Массив персонажей
+    const int numEnemies = 3;
+    Enemy* enemies[numEnemies];
+    enemies[0] = new Lancer("Лансер1", 10);
+    enemies[1] = new Dragon("Дракон1", 20);
+    enemies[2] = new Lancer("Лансер2", 15);
+
+    for (int i = 0; i < numEnemies; ++i) {
+        cout << "Персонаж " << i + 1 << ": " << endl;
+        enemies[i]->attack();
+    }
+
+    enemies[0]->attack();
+    enemies[1]->attack();
 
     return 0;
 }
