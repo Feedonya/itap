d6789e464f6959647209773a0b584c6a67aadcab
Fedor Musatov <145389323+Feedonya@users.noreply.github.com>
2024-04-23 11:11:33 +0400

    Update 9.cpp

--- Second_term/class_9/9.cpp
+++ Second_term/class_9/9.cpp
@@ -1,60 +1,121 @@
+
 #include <iostream>
 #include <string>
 
 using namespace std;
 
-class Enemy{
-protected:
+class Person{
+private:
     string name;
-    int attackPower;
+    string second_name;
 
 public:
-    // Êîíñòðóêòîð
-    Enemy(const string& _name, int _attackPower) : name(_name), attackPower(_attackPower){}
-
-    virtual void attack() const = 0;
-
-    // Äåñòðóêòîð
-    virtual ~Enemy(){}
-};
-
-class Lancer : public Enemy{
-public:
-    // Êîíñòðóêòîð
-    Lancer(const string& _name, int _attackPower) : Enemy(_name, _attackPower){}
-
-    // Ïåðåîïðåäåëåíèå ôóíêöèè attack
-    void attack() const override{
-        cout << "Ðûöàðü " << name << " àòàêóåò" << endl;
-    }
-};
-
-class Dragon : public Enemy{
-public:
-    // Êîíñòðóêòîð
-    Dragon(const string& _name, int _attackPower) : Enemy(_name, _attackPower){}
-
-    // Ïåðåîïðåäåëåíèå ôóíêöèè attack
-    void attack() const override{
-        cout << "Äðàêîí " << name << " àòàêóåò" << endl;
-    }
-};
-
-int main() {
-    // Ìàññèâ ïåðñîíàæåé
-    const int numEnemies = 3;
-    Enemy* enemies[numEnemies];
-    enemies[0] = new Lancer("Ëàíñåð1", 10);
-    enemies[1] = new Dragon("Äðàêîí1", 20);
-    enemies[2] = new Lancer("Ëàíñåð2", 15);
-
-    for (int i = 0; i < numEnemies; ++i){
-        cout << "Ïåðñîíàæ " << i + 1 << ": " << endl;
-        enemies[i]->attack();
+    Person(){
+		name = "No"; 
+		second_name = "Name";
     }
 
-    enemies[0]->attack();
-    enemies[1]->attack();
+    Person(string n, string s){
+		name = n;
+		second_name = s;
+    }
 
-    return 0;
+    string GetName(){
+		return name + " " + second_name;
+	}
+
+    bool operator > (Person s1){
+		return name > s1.name;
+	}
+	bool operator < (Person s1){
+		return name < s1.name;
+	}
+	bool operator == (Person s1){
+		return name == s1.name;
+	}
+	
+};
+
+class Song{
+    private:
+		Person performer;
+		Person song_author;
+		Person lyrics_author;
+		string lyrics;
+		static int count;	
+    public:
+    Song(Person p, Person s, Person l, string lyr){
+		performer = p;
+		song_author = s;
+		lyrics_author = l;
+		lyrics = lyr;
+		count++;
+    }
+
+    void ChangeLyrics(string nLyrics){
+    	lyrics = nLyrics;
+    }
+
+    string GetPerformer(){
+    	return performer.GetName();
+    }
+
+    string GetSongAuthor(){
+    	return song_author.GetName();
+    }
+
+    string GetLyricsAuthor(){
+    	return lyrics_author.GetName();
+    }
+
+    string GetLyrics(){
+    	return lyrics;
+    }
+
+	static int GetCount(){
+    	return count;
+    }
+    
+    string GetSongInfo(){
+		string temp;
+		temp.append("Èñïîëíèòåëü: ").append(GetPerformer()).append(" Àâòîð ìóçûêè: ").append(GetSongAuthor()).append(" Àâòîð ñëîâ: ").append(GetLyricsAuthor()).append(" Òåêñò ïåñíè: ").append(GetLyrics()).append("\n");
+		return temp;
+  	}
+  	
+  	bool operator > (Song s1){
+		return performer > s1.performer;
+	}
+	bool operator < (Song s1){
+		return performer < s1.performer;
+	}
+	bool operator == (Song s1){
+		return performer == s1.performer;
+	}
+};
+
+
+int Song::count = 0;
+
+int main(){
+    setlocale(LC_ALL, "Russian");
+
+	Person p1("Juju", "Baba");
+	Person p2("Sergey", "Lazarev");
+
+	Song s1(p1, p1, p1, "bbebebebebebebebebebeebebebeb");
+	Song s2(p1, p1, p1, "aaaaaaaaa alalalalalal");
+	Song s3(p2, p2, p2, "eifioqfoqhfjqbkjfbjkqbfjkqbfbqfkbj");
+
+	cout << (s1 == s2);
+	cout << '\n';
+
+	cout << s3.GetCount() << '\n';
+	
+	cout << s1.GetSongInfo();
+	
+	s1.ChangeLyrics("hello");
+	
+	cout << s1.GetSongInfo();
+
+	return 0;
 }
